bca5532866ea8e05d49130526b2f0be73c9ae3ec
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-04-20 00:14:24 +0200

    Add files via upload

---
+++ Zadatak-Vje-ba/Zad_vježba_1/CompetitionEntry.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_vježba_1
+{
+    internal class CompetitionEntry
+    {
+        Teacher teacher;
+        Dessert dessert;
+        List<Student> students = new List<Student>();
+        int[] ocjene = new int[3];
+        public int ratings=0;
+
+        public CompetitionEntry()
+        {
+
+        }
+        public CompetitionEntry(Teacher teacher, Dessert dessert, List<Student> students, int[] ocjene)
+        {
+            this.teacher = teacher;
+            this.dessert = dessert;
+            this.students = students;
+            this.ocjene = ocjene;
+        }
+
+        public CompetitionEntry(Teacher teacher, Dessert dessert)
+        {
+            this.teacher = teacher;
+            this.dessert = dessert;
+        }
+
+        public int[] Ocjene { get => ocjene; set => ocjene = value; }
+        internal Teacher Teacher { get => teacher; set => teacher = value; }
+        internal Dessert Dessert { get => dessert; set => dessert = value; }
+        internal List<Student> Students { get => students; set => students = value; }
+
+        public bool AddRating(Student student, int grade)
+        {
+            bool newStudent = true;
+            foreach (Student ucenik in students)
+            {
+                if (ucenik != student)
+                {
+                    newStudent = true;
+                }
+                else
+                {
+                    newStudent = false;
+                    break;
+                }
+            }
+            if (newStudent == true)
+            {
+                if (ratings >= 3)
+                {
+                    return false;
+                }
+                else
+                {
+                    students.Add(student);
+                    ocjene[ratings] = grade;
+                    ratings++;
+                    return true;
+
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetRating()
+        { 
+            int ukupno=0;
+            
+            foreach(int ocjena in ocjene)
+            {
+                ukupno += ocjena;
+            }
+            double prosjecna = ukupno / 3;
+            return prosjecna;
+        }
+    }
+}
---
+++ Zadatak-Vje-ba/Zad_vježba_1/UniMasterChef.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_vježba_1
+{
+    internal class UniMasterChef
+    {
+        CompetitionEntry entry;
+        int numberOfEnteries;
+        List<CompetitionEntry> entries = new List<CompetitionEntry>();
+        public bool newEntry = true;
+        public int upisane = 0;
+        List<Person> osobe = new List<Person>();
+
+        public UniMasterChef (int numberOfEnteries)
+        {
+            
+            this.numberOfEnteries = numberOfEnteries;
+        }
+
+        public int NumberOfEnteries { get => numberOfEnteries; set => numberOfEnteries = value; }
+
+        public bool AddEntery(CompetitionEntry entry)
+        {
+            foreach (CompetitionEntry ulaz in entries)
+            {
+                if (ulaz != entry)
+                {
+                    newEntry = true;
+                }
+                else
+                {
+                    newEntry = false;
+                    break;
+                }
+            }
+            if (upisane >= numberOfEnteries)
+            {
+                return false;
+            }
+            else
+            {
+                entries.Add(entry);
+                upisane++;
+                return true;
+            }
+        }
+        public CompetitionEntry getBestDesert()
+        {
+            CompetitionEntry best = new CompetitionEntry();
+            
+            foreach(CompetitionEntry entry in entries)
+            {
+                if (entry.GetRating() > best.GetRating())
+                {
+                    best = entry;
+                }
+            }
+
+            return best;
+        }
+        public List<Person> getInvolvedPeople(CompetitionEntry entry)
+        {
+            osobe.Clear();
+            foreach(Student student in entry.Students)
+            {
+                osobe.Add(student);
+            }
+            osobe.Add(entry.Teacher);
+
+            return osobe;
+        }
+    }
+}
